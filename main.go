5e3aee9ad801e7eaeb780ecb9f02c3976d9cd3bc
Lina <linamuhab@gmail.com>
2023-06-21 22:40:05 +0200

    Add timer for all of quiz

--- main.go
+++ main.go
@@ -8,15 +8,19 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
-func getInput(prompt string, reader *bufio.Reader) (string, error) {
+func getInput(prompt string, reader *bufio.Reader, answerChannel chan string) {
 	fmt.Print(prompt)
-	input, err := reader.ReadString('\n')
-	return strings.TrimSpace(input), err
+	input, _ := reader.ReadString('\n')
+	answerChannel <- strings.TrimSpace(input)
 }
 func main() {
 	fileName := flag.String("file", "problems.csv", "csv file in format of 'question,answer'")
+	timerValue := flag.Int("limit", 30, "Time limit for each question")
+	timerDuration := time.Duration(*timerValue) * time.Second
+	timer := time.NewTimer(timerDuration)
 	flag.Parse()
 
 	fmt.Println("Welcome to the super smart quiz game!")
@@ -29,11 +33,20 @@ func main() {
 	maxScore := len(records)
 	score := 0
 	reader := bufio.NewReader(os.Stdin)
+
+	answerChannel := make(chan string)
 	for _, record := range records {
-		userAnswer, _ := getInput(record[0], reader)
-		if strings.EqualFold(userAnswer, record[1]) {
-			score++
+		go getInput(record[0], reader, answerChannel)
+		select {
+		case answer := <-answerChannel:
+			if strings.EqualFold(answer, record[1]) {
+				score++
+			}
+		case <-timer.C:
+			fmt.Printf("\nCongratulations your score is: %d/%d\n", score, maxScore)
+			return
 		}
+
 	}
-	fmt.Printf("Congratulations your score is: %d/%d\n", score, maxScore)
+	fmt.Printf("\nCongratulations your score is: %d/%d\n", score, maxScore)
 }
