1824dab9f20bf6cc5dfca3d0e0689a77dc0170c9
Lina <linamuhab@gmail.com>
2023-07-04 22:07:50 +0200

    create separate directory for each gophercise

--- 1.Quiz Game/main.go
+++ 1.Quiz Game/main.go
@@ -11,17 +11,32 @@ import (
 	"time"
 )
 
+const (
+	HARD   = "hard"
+	NORMAL = "normal"
+)
+
 func getInput(prompt string, reader *bufio.Reader, answerChannel chan string) {
 	fmt.Print(prompt)
 	input, _ := reader.ReadString('\n')
 	answerChannel <- strings.TrimSpace(input)
 }
+
+// difficulty to switch between ticker and ticker
 func main() {
 	fileName := flag.String("file", "problems.csv", "csv file in format of 'question,answer'")
-	timerValue := flag.Int("limit", 5, "Time limit for each question")
-	tickerDuration := time.Duration(*timerValue) * time.Second
-	ticker := time.NewTicker(tickerDuration)
+	difficulty := flag.String("difficulty", NORMAL, "Difficulty setting for the quiz")
+	var defaultTimer int
+	if *difficulty == HARD {
+		defaultTimer = 5
+	} else {
+		defaultTimer = 90
+	}
+	timerValue := flag.Int("limit", defaultTimer, "Time limit for each question")
+
+	timerDuration := time.Duration(*timerValue) * time.Second
 	flag.Parse()
+	fmt.Println(*fileName, *difficulty, *timerValue)
 
 	fmt.Println("Welcome to the super smart quiz game!")
 
@@ -35,6 +50,9 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	answerChannel := make(chan string)
+
+	timer := time.NewTimer(timerDuration)
+
 	for _, record := range records {
 		go getInput(record[0], reader, answerChannel)
 		select {
@@ -42,8 +60,11 @@ func main() {
 			if strings.EqualFold(answer, record[1]) {
 				score++
 			}
-		case <-ticker.C:
-			fmt.Printf("\nTimer's up! Next Question")
+		case <-timer.C:
+			fmt.Printf("\nTimer's up! Next Question\n")
+			if *difficulty == HARD {
+				timer.Reset(timerDuration)
+			}
 			break
 		}
 
