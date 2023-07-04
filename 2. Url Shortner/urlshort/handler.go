784ac815088a329bc57a730ab239373d5df206d8
Lina <linamuhab@gmail.com>
2023-07-04 22:10:03 +0200

    create url shortner

---
+++ 2. Url Shortner/urlshort/handler.go
@@ -0,0 +1,80 @@
+package urlshort
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"net/http"
+)
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+
+	return func(writer http.ResponseWriter, request *http.Request) {
+		url, ok := pathsToUrls[request.URL.Path]
+		if ok {
+			http.Redirect(writer, request, url, http.StatusPermanentRedirect)
+		} else {
+			fallback.ServeHTTP(writer, request)
+		}
+	}
+}
+
+//
+//type YAMLInput struct {
+//	YAML struct {
+//		YAML []YAML `yaml:"path,url"`
+//	}
+//}
+
+type YAML struct {
+	Path string `yaml:"path"`
+	Url  string `yaml:"url"`
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//   - path: /some-path
+//     url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	//yamlData := YAML{}
+	//err := yaml.Unmarshal(yml, yamlData)
+	//fmt.Println(yamlData)
+	//return nil, err
+	parsedYaml, err := parseYAML(yml)
+	if err != nil {
+		return nil, err
+	}
+	pathMap := buildMap(parsedYaml)
+	return MapHandler(pathMap, fallback), nil
+}
+
+func parseYAML(yml []byte) ([]YAML, error) {
+	var parsedYAML []YAML
+	err := yaml.Unmarshal(yml, &parsedYAML)
+	return parsedYAML, err
+}
+func buildMap(parsedYAML []YAML) map[string]string {
+	fmt.Println(parsedYAML)
+	yamlMap := make(map[string]string)
+	for _, v := range parsedYAML {
+		yamlMap[v.Path] = v.Url
+	}
+	return yamlMap
+}
